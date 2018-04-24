6893dc0f4098a2221ce6c4a41fb16f81f55b6827
Mateusz Mazurkiewicz <7u5t6y@gmail.com>
2018-04-24 23:36:05 +0200

    Add simple list of items with onClick event handler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash'
 
 class App extends Component {
+  state = {
+    item: null
+  }
   render() {
     return (
       <div className="App">
-        Przykladowy tekst
+        <ul style={{listStyleType:'none'}}>
+          {
+            _.times(10).map(n => (
+              <li style={{cursor:'pointer'}}
+              onClick={() => this._onItemClick(n)}>
+                {`Strona: ${n}.`}
+              </li>
+            ))
+          }
+        </ul>
+        {
+          this.state.item && (
+            <strong>
+              {`Aktywny item: ${this.state.item}`}
+            </strong>
+          )
+        }
       </div>
     );
+  }
+  _onItemClick = n => {
+    this.setState({
+      ...this.state,
+      item: n
+    })
   }
 }
 
